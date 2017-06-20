76ed2296ffa59affbff65e1722ad80ac38b5d152
Mugesh18 <mugeshromper18@gmail.com>
2017-06-20 10:02:30 +0530

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,12 @@
+
+public class Index {
+	public static void main(String[] args){
+		int a[]={-3,-1,1,3,5};
+		for(int i=0;i<a.length;i++){
+			if(a[i]==3){
+				System.out.println(i);
+			}
+		}
+	}
+
+}
