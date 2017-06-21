4ca333ca6f231143c6d0a2325e7a443d5db4728f
Mugesh18 <mugeshromper18@gmail.com>
2017-06-21 12:24:10 +0530

    Add files via upload

---
+++ Holiday.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Holiday {
+	public static void main(String []args){
+		Scanner s=new Scanner(System.in);
+		String[] sc={"monday","tuesday","Wednesday","Thursday","Friday"};
+		String[] a={"Saturday","sunday"};
+		int n=s.nextInt();
+			if(n<=5)
+			{
+					System.out.println("True");
+				}
+			else{
+		System.out.println("False");
+	}}
+}
