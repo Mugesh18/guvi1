6b15b9a47d2791150e4072b7970d0fbe401bcb43
Mugesh18 <mugeshromper18@gmail.com>
2017-06-21 09:18:23 +0530

    Add files via upload

---
+++ Elements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Elements {
+	public static void main(String[] args){
+	 System.out.println("ENTER THE NUMBER OF ELEMENTS IN ARRAY: ");
+	   Scanner s=new Scanner(System.in);
+	   int n=s.nextInt();
+	   int[] Arr=new int[n];
+	   System.out.println("ENTER THE ELEMENTS:");
+	   for(int i=0;i<n;i++)
+	    {
+	     Arr[i]=s.nextInt();
+	    }
+	   for(int i=0;i<n;i++)
+	   {
+	       for(int j=0;j<n;j++)
+	       {
+	           for(int k=0;k<n;k++)
+	            {
+	              if((Arr[i]+Arr[j])==Arr[k])
+	                System.out.println("ELEMENT i ="+Arr[i]+"j="+Arr[j]+"k="+Arr[k]);
+	          
+	           
+	       }}
+	     }  
+}
+}
