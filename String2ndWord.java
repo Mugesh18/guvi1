60bc530d026493b39fb308b99ca40da7b921c6a1
Mugesh18 <mugeshromper18@gmail.com>
2017-06-25 12:12:23 +0530

    Add files via upload

---
+++ String2ndWord.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class String2ndWord {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		String Word[]=scan.nextLine().split(" "); 
+		System.out.println(Word[1]+"("+Word[1].length()+")");
+	}
+
+}
