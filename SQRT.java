bc4baaf5c30a5da8817ff07914a0e5e2775237c1
Mugesh18 <mugeshromper18@gmail.com>
2017-06-25 10:06:54 +0530

    Add files via upload

---
+++ SQRT.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class SQRT {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n : ");
+		int a1=10;
+		int a2=20;
+		int b1=3;
+		int b2=54;
+		int s;
+		s=(int) Math.sqrt(a1*a2+b1*b2);
+		System.out.println(s);
+		
+	}
+}
