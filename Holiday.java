7ddcdac2b0199dc643af02fed2dfef95b79f0893
Mugesh18 <mugeshromper18@gmail.com>
2017-06-28 15:05:21 +0530

    Add files via upload

--- Holiday.java
+++ Holiday.java
@@ -2,14 +2,40 @@ import java.util.Scanner;
 public class Holiday {
 	public static void main(String []args){
 		Scanner s=new Scanner(System.in);
-		String[] sc={"monday","tuesday","Wednesday","Thursday","Friday"};
-		String[] a={"Saturday","sunday"};
-		int n=s.nextInt();
-			if(n<=5)
-			{
-					System.out.println("True");
-				}
-			else{
-		System.out.println("False");
-	}}
+		String sc=s.nextLine();
+		if((sc.equalsIgnoreCase("Saturday") || sc.equalsIgnoreCase("Sat") ))
+		{
+			System.out.println(sc+"=False");
+		}
+		else if(sc.equalsIgnoreCase("Sunday")|| sc.equalsIgnoreCase("Sun"))
+		{
+			System.out.println(sc+"=False");
+		}
+		if((sc.equalsIgnoreCase("Monday") || sc.equalsIgnoreCase("mon") ))
+		{
+			System.out.println(sc+"=True");
+		}
+		else if(sc.equalsIgnoreCase("tuesday")|| sc.equalsIgnoreCase("tue"))
+		{
+			System.out.println(sc+"=True");
+		}
+		if((sc.equalsIgnoreCase("wednesday") || sc.equalsIgnoreCase("wed") ))
+		{
+			System.out.println(sc+"=True");
+		}
+		else if(sc.equalsIgnoreCase("thursday")|| sc.equalsIgnoreCase("thu"))
+		{
+			System.out.println(sc+"=True");
+		}
+		if((sc.equalsIgnoreCase("friday") || sc.equalsIgnoreCase("fri") ))
+		{
+			System.out.println(sc+"=True");
+		}
+		else
+		{
+			System.out.println(sc+"=Invalid Input");
+		}
+			
+		
+	}
 }
