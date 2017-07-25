5f15d087d50cb2fb40540e82f5c5759185c6dd89
Mugesh18 <mugeshromper18@gmail.com>
2017-07-25 11:11:45 +0530

    Add files via upload

--- Hunter123.java
+++ Hunter123.java
@@ -11,17 +11,12 @@ public static void main(String[] args) {
 	char ch[]=s.toCharArray();
 	for(int i=0;i<ch.length;i++)
 	{
-		for(int j=0;j<ch.length;j++)
-		{
+		for(int j=0;j<ch.length;j++) {
 			if(ch[i]==ch[j])
-			{
 				count++;
-			}
 		}
 		if(count<2)
-		{
 			System.out.print(ch[i]);
-		}
 		count=0;
 	}
 }
