2c5514807f6e925a295147e9ef93f64ed5a93391
Mugesh18 <mugeshromper18@gmail.com>
2018-07-05 21:13:10 +0530

    Add files via upload

--- SquareValue.java
+++ SquareValue.java
@@ -5,23 +5,11 @@ public class SquareValue {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter any value to find it is a factorial no. or not : ");
+		System.out.println("Enter a value which to be squared : ");
 		int n=sc.nextInt();
-		int b=1;
-		int a;
-		for(int i=1;i<=n;i++) {
-			a=b*i;
-			b=a;
-			
-			if(b==n) {
-				System.out.print("It is in a factorial "+i);
-				break;
-			}
-			else if(b>n){
-				System.out.print("It is not in a factorial");
-				break;
-		}
-		}
+		System.out.println("Enter Square in number :");
+		int s=sc.nextInt();
+			System.out.println(Math.pow(n,s));
 	}
 
 }
